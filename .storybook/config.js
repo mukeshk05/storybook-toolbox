8f91a04d8ec422fcb8ef1af913509286f43ff20f
Mukesh Kumar Singh <mukesh.kumar.s@outlook.com>
2019-09-03 22:40:56 -0500

    UI testing

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/react';
 import requireContext from 'require-context.macro';
 import 'react-chromatic/storybook-addon';
-
+import 'storybook-chromatic';
 import '../src/index.css';
 
 const req = requireContext('../src/components', true, /\.stories\.js$/);
