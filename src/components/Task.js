2200bed7a4f09332ba761a3889add318197aa2cc
Mukesh Kumar Singh <mukesh.kumar.s@outlook.com>
2019-09-03 23:02:32 -0500

    build 2

--- src/components/Task.js
+++ src/components/Task.js
@@ -13,7 +13,7 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
         />
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
-      <div className="title">
+      <div className="title" style={{background:'red'}}>
         <input type="text" value={title} readOnly={true} placeholder="Input title" style={{ 'text-overflow': 'ellipsis' }} />
       </div>
 
